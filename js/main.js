d380cbf9c0a8449317d58c61eed4052a5b6d170f
takuma <takuma.y0524@gmail.com>
2017-06-19 02:49:51 +0900

    Fixed bug

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-	let playerWhite = new Player(DISK_WHITE);
 	let playerBlack = new Player(DISK_BLACK);
+	let playerWhite = new Player(DISK_WHITE);
 
 	const interval = 0;
 
-	const white = function() {
-		if(playerWhite.place()) {
-			setTimeout(black, interval);
-		}else{
-			setTimeout(white, interval);
-		}
-	}
 	const black = function() {
 		if(playerBlack.place()) {
 			setTimeout(white, interval);
 		}else{
 			setTimeout(black, interval);
+		}
+	}
+	const white = function() {
+		if(playerWhite.place()) {
+			setTimeout(black, interval);
+		}else{
+			setTimeout(white, interval);
 		}
 	}
 
